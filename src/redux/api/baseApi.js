70af5a771326ac1453393c783b48dff37b709e6f
Mustafiz82e <mustafiz8260@gamil.com>
2024-07-03 16:20:54 +0600

    some bug fixed

--- src/redux/api/baseApi.js
+++ src/redux/api/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
 	reducerPath: "api",
-	baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5144" }),
+	baseQuery: fetchBaseQuery({ baseUrl: "https://bikroyelectronics-server.vercel.app/"  }),
 	tagTypes:[ 'user' , 'products'],
 	endpoints: (builder) => ({
 		getCategoryList: builder.query({
