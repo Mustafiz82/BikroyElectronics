9ef9e89a095149b3e19c8a59936aa63afa9aea3d
Mustafiz82 <mustafiz8260@gmail.com0>
2024-08-21 23:44:37 +0600

    contack us form daynamic via emailjs

--- src/pages/Contack/Contack.jsx
+++ src/pages/Contack/Contack.jsx
@@ -1,18 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoCallOutline } from "react-icons/io5";
 import { IoMailOutline } from "react-icons/io5";
+import emailjs from '@emailjs/browser';
 
 const Contack = () => {
 
-	const handleContackUs = (e) => {
-		e.preventDefault()
-		const name = e.targer.name.value
-		const phone = e.targer.phone.value
-		const email = e.targer.email.value
-		const message = e.targer.message.value
+	const [btnText , setBtnText]  = useState("Send Message")
 
-		
-		
+	const handleContackUs = (e) => {
+
+		setBtnText("Sending ...")
+		e.preventDefault()
+
+		const name = e.target.name.value
+		const phone = e.target.phone.value
+		const email = e.target.email.value
+		const message = e.target.message.value
+
+		const serviceId = "service_yev8zd8"
+		const templeteId = "template_6xulcht"
+		const publicKey = "1basqFKWSgsXJyEOu"
+
+
+		const templateParams = {
+			from_name: name,
+			from_email: email,
+			from_phone: phone,
+			message: message,
+			to_email: "mustafiz8260@gmail.com",
+			to_name:"Mustafiz"
+		}
+
+
+		console.log(name, phone, email, message)
+
+		emailjs
+			.send(serviceId, templeteId, templateParams, publicKey)
+			.then(
+				() => {
+					console.log('SUCCESS!');
+					setBtnText("Email Sent")
+				},
+				(error) => {
+					console.log('FAILED...', error.text);
+					setBtnText("Send Failed")
+				},
+			);
+
 	}
 	return (
 		<div className="max-w-screen-xl flex flex-col lg:flex-row px-5 lg:px-0 gap-2 lg:gap-20  mx-auto my-5 lg:my-20">
@@ -48,32 +82,35 @@ const Contack = () => {
 					</div>
 				</div>
 			</div>
-			<div className="w-full ">
+			<form onSubmit={handleContackUs} className="w-full ">
 				<div className="flex  gap-5">
 					<input
 						type="text"
-						placeholder="Name"required
+						placeholder="Name" required
 						className=" input focus:border-none focus:outline-none  rounded-sm w-full  bg-[#F5F5F5]"
+						name="name"
 					/>
 					<input
 						type="tel"
-						placeholder="Phone"required
+						placeholder="Phone" required
 						className=" input focus:border-none focus:outline-none  rounded-sm w-full  bg-[#F5F5F5]"
+						name="phone"
 					/>
 					<input
 						type="email"
-						placeholder="Email"required
+						placeholder="Email"
 						className=" input focus:border-none focus:outline-none  rounded-sm w-full  bg-[#F5F5F5]"
+						name="email"
 					/>
 				</div>
-			<div className="mt-5">
-				<textarea placeholder="Your Message" required className="textarea resize-none focus:border-none focus:outline-none rounded-sm outline-none w-full h-64  bg-[#F5F5F5]" name="" id=""></textarea>
-<div className="flex justify-end">
-<button className="btn btn-error bg-primary  px-8  mt-5 rounded-sm text-white ">Send Message</button>
+				<div className="mt-5">
+					<textarea placeholder="Your Message" required className="textarea resize-none focus:border-none focus:outline-none rounded-sm outline-none w-full h-64  bg-[#F5F5F5]" name="message" id=""></textarea>
+					<div className="flex justify-end">
+						<button className="btn btn-error bg-primary  px-8  mt-5 rounded-sm text-white ">{btnText}</button>
 
-</div>
-			</div>
-			</div>
+					</div>
+				</div>
+			</form>
 		</div>
 	);
 };
